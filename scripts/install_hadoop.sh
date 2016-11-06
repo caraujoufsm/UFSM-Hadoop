2220a9658ba4cab4c97fb619b33ea83dbc5cfa29
icunhacorrea <iago.cc@outlook.com>
2016-11-06 14:18:35 -0200

    Backup at Dom Nov  6 14:18:35 BRST 2016

--- scripts/install_hadoop.sh
+++ scripts/install_hadoop.sh
@@ -4,13 +4,6 @@ cd
 wget https://archive.apache.org/dist/hadoop/common/hadoop-2.7.1/hadoop-2.7.1.tar.gz
 mv hadoop-2.7.1.tar.gz hadoop
 
-cat export HADOOP_HOME=/home/hadoop/hadoop >> .bashrc
-cat export HADOOP_CLASSPATH=/usr/lib/jvm/java-8-oracle/lib/tools.jar >> .bashrc
-cat export HADOOP_INSTALL=$HADOOP_HOME >> .bashrc
-cat export HADOOP_MAPRED_HOME=$HADOOP_HOME >> .bashrc
-cat export HADOOP_COMMON_HOME=$HADOOP_HOME >> .bashrc
-cat export HADOOP_HDFS_HOME=$HADOOP_HOME >> .bashrc
-cat export YARN_HOME=$HADOOP_HOME >> .bashrc
-cat export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native >> .bashrc
-cat export PATH=$PATH:$HADOOP_HOME/sbin:$HADOOP_HOME/bin >> .bashrc
-cat export HADOOP_OPTS="-Djava.library.path=$HADOOP_HOME/lib/native" >> .bashrc
+cp ozone_proj/scripts/bash/.bashrc ~/
+cp ozone_proj/scripts/sh/hadoop-env.sh /hadoop/etc/hadoop/
+cp ozone_proj/scripts/xml/*.xml /hadoop/etc/hadoop/
